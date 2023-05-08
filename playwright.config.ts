21678e23852e27db3aa430b5bc74c3d84ceb52a1
calm1205 <naginani072@gmail.com>
2023-05-08 21:30:38 +0900

    [rename] playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 const baseURL = "http://localhost:3000";
 
 export default defineConfig({
-  testDir: "__tests__",
+  testDir: "./src/__tests__",
   use: {
     baseURL,
     trace: "on-first-retry",
