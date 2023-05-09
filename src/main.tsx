d1bceac7853b4af811cbf5336daf2da24e8a4ae0
calm1205 <naginani072@gmail.com>
2023-05-09 19:36:14 +0900

    mswのsetup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { setupWorker } from "msw";
+import { coffeeMock } from "./__mocks__/coffee.mock.ts";
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const worker = setupWorker(coffeeMock);
+worker.start();
+// worker.start({ serviceWorker: { url: "" } });
 
 root.render(
   <React.StrictMode>
